de8ecdb63635d2cfcefb77286fc2cc5a60de5811
juneskim <101495950+juneskim@users.noreply.github.com>
2024-11-25 23:52:00 -0800

    red "not enough coins message + green "purchased message"

--- Fluffed Up/Assets/Scripts/ShopController.cs
+++ Fluffed Up/Assets/Scripts/ShopController.cs
@@ -18,6 +18,8 @@ public class ShopController: MonoBehaviour
     [SerializeField] private GameObject option1Name;
     [SerializeField] private GameObject option1Description;
     [SerializeField] private GameObject notEnoughCoinsText1; // Text for option 1
+    [SerializeField] private GameObject purchaseText1; // Text for option 1
+
 
     [Header("Option 2 UI Elements")]
     [SerializeField] private GameObject option2;
@@ -25,6 +27,9 @@ public class ShopController: MonoBehaviour
     [SerializeField] private GameObject option2Name;
     [SerializeField] private GameObject option2Description;
     [SerializeField] private GameObject notEnoughCoinsText2; // Text for option 2
+    [SerializeField] private GameObject purchaseText2; // Text for option 2
+
+
 
     [Header("Option 3 UI Elements")]
     [SerializeField] private GameObject option3;
@@ -32,6 +37,10 @@ public class ShopController: MonoBehaviour
     [SerializeField] private GameObject option3Name;
     [SerializeField] private GameObject option3Description;
     [SerializeField] private GameObject notEnoughCoinsText3; // Text for option 3
+    [SerializeField] private GameObject purchaseText3; // Text for option 3
+
+
+
 
     [Header("Consumable UI Elements")]
     [SerializeField] private GameObject consumableOption;
@@ -39,6 +48,10 @@ public class ShopController: MonoBehaviour
     [SerializeField] private GameObject consumableOptionName;
     [SerializeField] private GameObject consumableOptionDescription;
     [SerializeField] private GameObject notEnoughCoinsText4; // Text for consumable option
+    [SerializeField] private GameObject purchaseText4; // Text for option 4
+
+ 
+
 
     [Header("Sound Effects")]
     [SerializeField]
@@ -73,6 +86,12 @@ public class ShopController: MonoBehaviour
         notEnoughCoinsText2.SetActive(false);
         notEnoughCoinsText3.SetActive(false);
         notEnoughCoinsText4.SetActive(false);
+        purchaseText1.SetActive(false);
+        purchaseText2.SetActive(false);
+        purchaseText3.SetActive(false);
+        purchaseText4.SetActive(false);
+
+
 
         dropTables = dt.GetComponent<DropTables>();
         player = FindObjectOfType<PlayerController>();
@@ -124,6 +143,11 @@ public class ShopController: MonoBehaviour
         notEnoughCoinsText2.SetActive(false);
         notEnoughCoinsText3.SetActive(false);
         notEnoughCoinsText4.SetActive(false);
+
+        purchaseText1.SetActive(false);
+        purchaseText2.SetActive(false);
+        purchaseText3.SetActive(false);
+        purchaseText4.SetActive(false);
 
         Time.timeScale = 1f;
         Cursor.lockState = CursorLockMode.Locked;
@@ -236,23 +260,23 @@ public class ShopController: MonoBehaviour
         switch (i)
         {
             case 1:
-                AttemptPurchase(upgrade1, ref upgrade1Purchased, option1, notEnoughCoinsText1);
+                AttemptPurchase(upgrade1, ref upgrade1Purchased, option1, notEnoughCoinsText1, purchaseText1);
                 break;
             case 2:
-                AttemptPurchase(upgrade2, ref upgrade2Purchased, option2, notEnoughCoinsText2);
+                AttemptPurchase(upgrade2, ref upgrade2Purchased, option2, notEnoughCoinsText2, purchaseText2);
                 break;
             case 3:
-                AttemptPurchase(upgrade3, ref upgrade3Purchased, option3, notEnoughCoinsText3);
+                AttemptPurchase(upgrade3, ref upgrade3Purchased, option3, notEnoughCoinsText3, purchaseText3);
                 break;
             case 4:
-                AttemptPurchase(consumable, ref consumablePurchased, consumableOption, notEnoughCoinsText4);
+                AttemptPurchase(consumable, ref consumablePurchased, consumableOption, notEnoughCoinsText4, purchaseText4);
                 break;
             default:
                 break;
         }
     }
 
-    private void AttemptPurchase(Upgrade upgrade, ref bool isPurchased, GameObject option, GameObject notEnoughCoinsText)
+    private void AttemptPurchase(Upgrade upgrade, ref bool isPurchased, GameObject option, GameObject notEnoughCoinsText, GameObject purchaseText)
     {
         if (player.GetCoins() >= upgrade.cost)
         {
@@ -262,6 +286,7 @@ public class ShopController: MonoBehaviour
             isPurchased = true;
             HandleUpgrade(upgrade);
             PlayConfirmSound();
+            purchaseText.SetActive(true);
         }
         else
         {
